2761ccabc7cab8fa7f769414f82d987983e94019
Sushil <sushilrawat1720@gmail.com>
2023-03-04 23:37:39 +0530

    bug

--- utils/getJWTtoken.js
+++ utils/getJWTtoken.js
@@ -1,24 +1,25 @@
 //Creating token and saving  in cookie
 const sendToken = (user, statusCode, res) => {
-    const token = user.getJWTToken();
-    //option for cookies
-    const options = {
-        expires: new Date(
-            Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
-        ),
-        httpOnly: true,
-        // secure: true  ,
-        // secure: req.secure || req.headers["x-forwarded-proto"] === "https",
-        sameSite: "none",
-        // credentials: "same-origin",
-    };
-    // if (process.env.NODE_ENV === "production") options.secure = true;
+  const token = user.getJWTToken();
+  //option for cookies
+  const options = {
+    expires: new Date(
+      Date.now() + process.env.COOKIE_EXPIRE * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+    // path: "/",
+    secure: true,
+    // secure: req.secure || req.headers["x-forwarded-proto"] === "https",
+    // sameSite: "none",
+    // credentials: "same-origin",
+  };
+  // if (process.env.NODE_ENV === "production") options.secure = true;
 
-    res.status(statusCode).cookie("token", token, options).json({
-        success: true,
-        user,
-        token,
-    });
+  res.status(statusCode).cookie("token", token, options).json({
+    success: true,
+    user,
+    token,
+  });
 };
 
 module.exports = sendToken;
